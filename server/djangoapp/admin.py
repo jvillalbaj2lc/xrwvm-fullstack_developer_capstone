c66f3bd9a3bed7136a4644ec2f9a6e7006558cb1
Joseph <joseph.villalba@j2lc.com>
2025-01-03 12:05:32 -0500

    v4 complete

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,20 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
-# Register your models here.
+# Customizing CarMake admin
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'country', 'founded_year')  # Display these fields in the list view
+    search_fields = ('name', 'country')  # Add a search box for these fields
 
-# CarModelInline class
 
-# CarModelAdmin class
+# Customizing CarModel admin
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year', 'dealer_id', 'fuel_type')  # Fields to display in list view
+    list_filter = ('type', 'fuel_type', 'car_make')  # Add filters for these fields
+    search_fields = ('name', 'car_make__name')  # Add search functionality for car model name and car make name
 
-# CarMakeAdmin class with CarModelInline
 
-# Register models here
+# Registering models with their custom admin configurations
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,57 @@
 # Uncomment the following imports before adding the Model code
-
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
-
-
-# Create your models here.
-
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+# CarMake Model
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)  # Name of the car make
+    description = models.TextField()  # Description of the car make
+    country = models.CharField(max_length=100, blank=True, null=True)  # Optional: Country of origin
+    founded_year = models.IntegerField(
+        validators=[
+            MinValueValidator(1800),  # Assume car companies started around the 1800s
+            MaxValueValidator(now().year)
+        ],
+        blank=True,
+        null=True
+    )  # Optional: Year the company was founded
+
+    def __str__(self):
+        return f"{self.name} ({self.description})"  # String representation
+
+
+# CarModel Model
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    dealer_id = models.IntegerField(blank=True, null=True)  # Make dealer_id optional
+    name = models.CharField(max_length=100)  # Name of the car model
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('TRUCK', 'Truck'),
+        ('COUPE', 'Coupe'),
+        ('HATCHBACK', 'Hatchback'),
+    ]  # Choices for car type
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')  # Car type with limited choices
+    year = models.IntegerField(
+        validators=[
+            MaxValueValidator(2023),  # Ensure the year is within the range specified in the data
+            MinValueValidator(2015)
+        ]
+    )  # Year field with validation
+    fuel_type = models.CharField(
+        max_length=20,
+        choices=[
+            ('GAS', 'Gasoline'),
+            ('DIESEL', 'Diesel'),
+            ('ELECTRIC', 'Electric'),
+            ('HYBRID', 'Hybrid'),
+        ],
+        default='GAS'
+    )  # Optional: Type of fuel the car uses
+
+    def __str__(self):
+        return f"{self.car_make.name} {self.name} ({self.type}, {self.year})"  # String representation
